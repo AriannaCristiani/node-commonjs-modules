b17499071c90bd94c9ab6f5c9d67a40bcbe35e99
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-15 16:07:02 +0100

    creata la seconda funzione ed esportata nel file principale

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const userFullName = require("./functions");
+const userFullName = require("./name.js");
+const hobbies = require ("./hobbies.js")
 
 console.log(userFullName('Arianna', 'Cristiani'))
-
+console.log(hobbies('drawing', 'painting' , 'gardening'))
